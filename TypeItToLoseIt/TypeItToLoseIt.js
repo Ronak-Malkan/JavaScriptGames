d22dca66370af19eccb297802b3651d8fcc70376
Ronak-Malkan <ronakmalkan2062001@gmail.com>
2023-06-15 23:01:05 +0530

    feat: added letter bullets, accuracy, etc

--- TypeItToLoseIt/TypeItToLoseIt.js
+++ TypeItToLoseIt/TypeItToLoseIt.js
@@ -18,9 +18,14 @@ let spawnTimeRemaining = 0;
 
 let wordFallTime = 10;
 
+let letterBulletTransitionTime = 500;
+
 let mainInterval;
 
 let score = 0;
+
+let totalLettersTyped;
+let totalCorrectLetters;
 
 const root = document.querySelector(":root");
 const startButton = document.querySelector("#start");
@@ -28,17 +33,21 @@ const wordFallSpeedInput = document.querySelector("#wordFallSpeed");
 const wordSpawnSpeedInput = document.querySelector("#wordSpawnSpeed");
 const playground = document.querySelector(".playground");
 const scoreboard = document.querySelector(".scoreboard");
+const lostBoard = document.querySelector(".lostBoard");
+const restartButton = document.querySelector("#restart");
+const homeButton = document.querySelector("#home");
 
 // Event Listeners -------------------------------------------------------
 
 document.querySelector("body").addEventListener('click', changeTheme);
 startButton.addEventListener('click', start);
-
+restartButton.addEventListener('click', start);
+homeButton.addEventListener('click', showGameIntro);
 
 // Event Handlers --------------------------------------------------------
 
 function changeTheme(e){
-   if(e.target != startButton && e.target != wordFallSpeedInput && e.target != wordSpawnSpeedInput){
+   if(e.target != startButton && e.target != wordFallSpeedInput && e.target != wordSpawnSpeedInput && e.target != restartButton && e.target != homeButton){
       if(colorScheme == 'dark'){
          colorScheme = 'light';
          root.style.colorScheme = 'light';
@@ -54,62 +63,117 @@ function changeTheme(e){
    }
 }
 
-function start(){
+function start(e){
+   for(let i=0; i<wordCreatedNode.length; i++){
+      wordCreatedNode[i].remove();
+   }
+
+   scoreboard.textContent = '0';
+
+   wordCreatedList = [];
+
+   wordCreatedNode = [];
+
+   playground.innerHTML = '';
+
+   wordListIndex = -1;
+   wordIndex = 0;
+
+   isGameOver = false;
+   spawnTimeRemaining = 0;
+   score = 0;
+   totalCorrectLetters = 0;
+   totalLettersTyped = 0;
+
+   document.querySelector("body").removeEventListener("click", changeTheme);
+
    document.querySelector(".gameIntro").style.display = 'none';
    document.querySelector(".containerForBlur").style.display = 'none';
-   if(wordFallSpeedInput.value != null && wordFallSpeedInput.value != 10) {
+   lostBoard.style.display = "none";
+   if(e.target == startButton && wordFallSpeedInput.value != null && wordFallSpeedInput.value != 10) {
       wordFallTime = wordFallSpeedInput.value;
    }
-   if(wordSpawnSpeedInput.value != null && wordSpawnSpeedInput.value != 2.0) {
+   if(e.target == startButton && wordSpawnSpeedInput.value != null && wordSpawnSpeedInput.value != 2.0) {
       spawnInterval = wordSpawnSpeedInput.value;
    }
 
    document.addEventListener("keydown", typing);
 
+   clearInterval(mainInterval);
+
    setTimeout(() => {
       mainInterval = setInterval(controller, 20);
-   }, 1000)
+   }, 1000); 
 }
 
 
 function typing(e){
+   let temp, tempWord, wordEnd;
+   if(e.key != "Shift"){
+      totalLettersTyped++;
+   }
    if(wordListIndex != -1){
       if(e.key == wordCreatedList[wordListIndex][wordIndex]){
+         totalCorrectLetters++;
+         shootLetter(e.key, wordCreatedNode[wordListIndex]);
          wordIndex++;
-         wordCreatedNode[wordListIndex].textContent = wordCreatedList[wordListIndex].slice(wordIndex);
-      }
-      console.log(wordCreatedList[wordListIndex] ,wordCreatedList[wordListIndex].length);
-      if(wordIndex == wordCreatedList[wordListIndex].length){
+         temp = wordCreatedNode[wordListIndex];
+         tempWord = wordCreatedList[wordListIndex].slice(wordIndex);
+         wordEnd = false;
+         if(wordIndex == wordCreatedList[wordListIndex].length){
 
-         updateScore(wordCreatedList[wordListIndex].length);
+            wordEnd = true;
 
-         wordCreatedNode[wordListIndex].remove();
-         wordCreatedList.splice(wordListIndex, 1);
-         wordCreatedNode.splice(wordListIndex, 1);
-         wordListIndex = -1;
-         wordIndex=0;
+            updateScore(wordCreatedList[wordListIndex].length);
+   
+            wordCreatedList.splice(wordListIndex, 1);
+            wordCreatedNode.splice(wordListIndex, 1);
+            wordListIndex = -1;
+            wordIndex=0;
+         }
+         setTimeout(() => {
+            temp.textContent = tempWord;
+            if(wordEnd){
+               temp.remove();
+            }
+         }, letterBulletTransitionTime + 50)
       }
    }
    else {
       for(let i=0; i<wordCreatedList.length; i++){
          if(e.key == wordCreatedList[i][0]){
+            totalCorrectLetters++;
+            shootLetter(e.key, wordCreatedNode[i]);
             wordListIndex = i;
             wordIndex++;
-            wordCreatedNode[wordListIndex].textContent = wordCreatedList[wordListIndex].slice(wordIndex);
+            temp = wordCreatedNode[wordListIndex];
+            tempWord = wordCreatedList[wordListIndex].slice(wordIndex);
+            wordEnd = false
             if(wordIndex == wordCreatedList[wordListIndex].length){
-
                updateScore(1);
-
-               wordCreatedNode[wordListIndex].remove();
                wordCreatedList.splice(wordListIndex, 1);
                wordCreatedNode.splice(wordListIndex, 1);
                wordListIndex = -1;
                wordIndex=0;
+               wordEnd = true;
             }
+
+            setTimeout(() => {
+               temp.textContent = tempWord;
+               if(wordEnd){
+                  temp.remove();
+               }
+            }, letterBulletTransitionTime + 50)
             break;
          }
       }
    }
+}
+
+function showGameIntro(){
+   document.querySelector(".gameIntro").style.display = 'inline-block';
+   document.querySelector(".containerForBlur").style.display = 'inline-block';
+   lostBoard.style.display = "none";
 }
 
 
@@ -142,16 +206,47 @@ function createWord(){
 }
 
 function checkIfWordReachBottom(){
-   if( !isGameOver && wordCreatedNode[0] != null && wordCreatedNode[0].offsetTop >= playground.offsetHeight-wordCreatedNode[0].offsetHeight){
+   if(wordCreatedNode[0] != null && wordCreatedNode[0].offsetTop >= playground.offsetHeight-wordCreatedNode[0].offsetHeight){
+      wordCreatedNode[0].remove();
+      wordCreatedNode.shift();
+      wordCreatedList.shift();
       gameOver();
    }
 }
 
 function gameOver(){
-   isGameOver = true;
+   if(!isGameOver){
+      isGameOver = true;
+      document.removeEventListener("keydown", typing);
+      document.querySelector("body").addEventListener('click', changeTheme);
+      lostBoard.style.display = "inline-block";
+      document.querySelector(".containerForBlur").style.display = 'inline-block';
+      updateLostBoard();
+   }
+}
+
+function updateLostBoard(){
+   document.querySelector("#currentPoints").textContent = `${score}`;
+   document.querySelector("#currentAccuracy").textContent = `${(totalCorrectLetters*100/totalLettersTyped).toFixed(1)}%`;
 }
 
 function updateScore(points){
    score += points;
    scoreboard.textContent = score;
+}
+
+function shootLetter(letter, wordNode){
+   const letterBullet = document.createElement("div");
+   letterBullet.classList.add("letterBullet");
+   letterBullet.textContent = letter;
+   playground.append(letterBullet);
+   let fallSpeed = playground.offsetHeight/(wordFallTime*1000);
+   let top = wordNode.offsetTop + fallSpeed*(letterBulletTransitionTime+50);
+   setTimeout(() => {
+      letterBullet.style.top = `${top}px`;
+      letterBullet.style.left = `${wordNode.offsetLeft}px`;
+      setTimeout(() => {
+         letterBullet.remove();
+      }, letterBulletTransitionTime)
+   }, 50);
 }
